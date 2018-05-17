bc1fc38fd420c4501f529f7af3d9c854d3c29beb
stefaleon <stefaleon@gmail.com>
2018-05-17 15:39:11 +0300

    01 Generics

---
+++ 01 Generics/GenericDictionary/GenericDictionary/GenericDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace GenericDictionary
+{
+    public class GenericDictionary<TKey, TValue>
+    {
+        IList<GenericDictionaryItem<TKey, TValue>> _items = new List<GenericDictionaryItem<TKey, TValue>>();
+        public void AddItem(TKey key, TValue value)
+        {
+            _items.Add(new GenericDictionaryItem<TKey, TValue>(key, value));
+        }
+
+        public TValue GetTheValueForKey(TKey key)
+        { 
+            for (int i = 0; i < _items.Count; i++)
+            {
+                if (_items[i].GetKey().Equals(key))
+                    return _items[i].GetValue();
+            }
+
+            return default(TValue);
+        }
+
+        public TKey FindTheFirstKeyWithValue(TValue value)
+        {
+            for (int i = 0; i < _items.Count; i++)
+            {
+                if (_items[i].GetValue().Equals(value))
+                    return _items[i].GetKey();
+            }
+
+            return default(TKey);
+        }
+    }
+}
---
+++ 01 Generics/GenericDictionary/GenericDictionary/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book1 = new Book { Isbn = "", Title = "Brave New World", Author = "Aldous Huxley" };
+
+            var dictionary0 = new GenericDictionary<int, string>();
+            dictionary0.AddItem(98, "This is value98 in dictionary0.");
+            Console.WriteLine(dictionary0.GetTheValueForKey(98));
+
+            Console.WriteLine();
+            var dictionary1 = new GenericDictionary<int, Book>();            
+            dictionary1.AddItem(1001, book1);
+            dictionary1.AddItem(1002, new Book { Title = "Demons", Author = "Fyodor Dostoevsky" });
+            Console.WriteLine(dictionary1.GetTheValueForKey(1001).Title);
+            Console.WriteLine(dictionary1.GetTheValueForKey(1002).Title);
+
+            Console.WriteLine();
+            var dictionary2 = new GenericDictionary<string, Book>();            
+            dictionary2.AddItem("abcd1234", book1);
+            dictionary2.AddItem("some other string here", new Book { Title = "Some New Book" });
+            Console.WriteLine(dictionary2.GetTheValueForKey("abcd1234").Author);
+            Console.WriteLine(dictionary2.GetTheValueForKey("some other string here").Title);
+
+            Console.WriteLine();
+            var dictionary3 = new GenericDictionary<Book, string>();
+            dictionary3.AddItem(book1, "this is weird");
+            Console.WriteLine(dictionary3.GetTheValueForKey(book1));
+            Console.WriteLine(dictionary3.FindTheFirstKeyWithValue("this is weird").Title);
+
+            Console.WriteLine();
+        }
+
+
+       
+
+            
+    }
+}
